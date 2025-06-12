14112327942edf5be9735334a2e19fbbcc7ed8ed
aanujkhurana <aanujkhurana@gmail.com>
2025-06-12 20:07:11 +1000

    added links on exp

--- frontend/src/components/Experiences.vue
+++ frontend/src/components/Experiences.vue
@@ -17,18 +17,94 @@ onMounted(() => {
 </script>
 
 <template>
-  <ol class="timeline-list">
-    <template v-for="experience in experiences" :key="experience.year">
-      <li
-        v-for="work in experience.works"
-        :key="work.date + work.desc"
-        class="timeline-item"
-      >
-        <h4 class="h4 timeline-item-title">{{ work.name }}</h4>
-        <span class="timeline-text">{{ experience.year }}</span>
-        <p class="timeline-text">{{ work.company }} | {{ work.desc }}</p>
-        <!-- <p class="timeline-text"></p> -->
+  <header>
+    <h2 class="h2 article-title">Experience</h2>
+  </header>
+
+  <section class="timeline">
+    <div class="title-wrapper">
+      <div class="icon-box">
+        <ion-icon name="book-outline"></ion-icon>
+      </div>
+      <h3 class="h3">Experience</h3>
+    </div>
+    <ol class="timeline-list">
+      <template v-for="experience in experiences" :key="experience.year">
+        <li
+          v-for="work in experience.works"
+          :key="work.date + work.desc"
+          class="timeline-item"
+        >
+          <h4 class="h4 timeline-item-title">{{ work.name }}</h4>
+          <span class="timeline-text">{{ experience.year }}</span>
+          <p class="timeline-text">{{ work.company }} | {{ work.desc }}</p>
+          <!-- <p class="timeline-text"></p> -->
+        </li>
+      </template>
+    </ol>
+  </section>
+
+  <section class="timeline">
+    <div class="title-wrapper">
+      <div class="icon-box">
+        <ion-icon name="book-outline"></ion-icon>
+      </div>
+
+      <h3 class="h3">Education</h3>
+    </div>
+
+    <ol class="timeline-list">
+      <li class="timeline-item">
+        <h4 class="h4 timeline-item-title">Griffith University</h4>
+
+        <span>2024</span>
+
+        <p class="timeline-text">
+          Master of Information Technology in Software Development & Support.
+        </p>
       </li>
-    </template>
-  </ol>
+    </ol>
+  </section>
+
+  <section class="timeline">
+    <div class="title-wrapper">
+      <div class="icon-box">
+        <ion-icon name="book-outline"></ion-icon>
+      </div>
+
+      <h3 class="h3">Certificates</h3>
+    </div>
+
+    <ol class="timeline-list">
+      <li class="timeline-item">
+        <h4 class="h4 timeline-item-title">
+          <a href="https://www.hackerrank.com/certificates/fc67ad9cf98d">Python </a>
+        </h4>
+        <p class="timeline-text">HackerRank</p>
+      </li>
+      <li class="timeline-item">
+        <h4 class="h4 timeline-item-title">
+          <a
+            href="https://www.freecodecamp.org/certification/fcc8fef749d-61ec-4fed-95cf-dac26473a2b6/back-end-development-and-apis"
+            >Backend Development & API
+          </a>
+        </h4>
+        <p class="timeline-text">FreeCodeCamp</p>
+      </li>
+      <li class="timeline-item">
+        <h4 class="h4 timeline-item-title">
+          <a href="https://www.hackerrank.com/certificates/43ae65a24b0f">SQL</a>
+        </h4>
+        <p class="timeline-text">HackerRank</p>
+      </li>
+      <li class="timeline-item">
+        <h4 class="h4 timeline-item-title">
+          <a href="https://www.udemy.com/certificate/UC-KYJCWBQF/"
+            >User Experience Essentials</a
+          >
+        </h4>
+        <p class="timeline-text">Udemy</p>
+      </li>
+    </ol>
+  </section>
 </template>
