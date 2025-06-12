f58e496256f104096b6200d78ccfddd8d320e6d4
aanujkhurana <aanujkhurana@gmail.com>
2025-06-12 21:07:29 +1000

    backend sanity ⚙️

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'f26ecsn2',
+    dataset: 'production'
+  }
+})
--- react/frontend/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as NavBar } from './Navbar/Navbar';
-export { default as NavigationDots } from './NavigationDots';
-export { default as SocialMedia } from './SocialMedia';
